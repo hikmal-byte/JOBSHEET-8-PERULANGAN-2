e09988e30f13d6b7ee391d3632c674463997585d
hikmal-byte <hikmalfadillahrasyid@gmail.com>
2025-11-05 16:29:31 +0700

    P10_percobaan2

---
+++ Square14.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Square14 {
+    public static void main(String[] args) {
+        Scanner hikmal = new Scanner (System.in);
+        System.out.print("Masukkan nilai N = ");
+        int n14 = hikmal.nextInt();
+        for(int iOuter14=1; iOuter14<=n; iOuter14++) {
+            for (int i14 = 1; i14 <= n14; i14++) {
+                System.out.print("*");
+            }
+
+        }
+
+    }
+    
+}
